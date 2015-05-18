3600207713ddf6d88282f8397d83cd90f557ec1a
Nicholas Lee <voone97@yahoo.com>
2015-05-18 18:02:06 -0400

    fixed camDist

--- strawman/Assets/Scripts/Object Scripts/PlayerCamera.cs
+++ strawman/Assets/Scripts/Object Scripts/PlayerCamera.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class PlayerCamera : MonoBehaviour {
 	public GameObject target;
+	public float camDist;
 	// Use this for initialization
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3 (target.transform.position.x,
 		                                 target.transform.position.y,
-		                                 target.transform.position.z);
+		                                 target.transform.position.z-camDist);
 
 	}
 }
