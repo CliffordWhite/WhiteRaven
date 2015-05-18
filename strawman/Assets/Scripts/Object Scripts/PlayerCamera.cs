c4a7ada08b318ef1e4e32805e6e3760250cf8f9f
Nicholas Lee <voone97@yahoo.com>
2015-05-18 17:47:57 -0400

    camera follow player script

---
+++ strawman/Assets/Scripts/Object Scripts/PlayerCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+	public GameObject target;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (target.transform.position.x,
+		                                 target.transform.position.y,
+		                                 target.transform.position.z);
+
+	}
+}
