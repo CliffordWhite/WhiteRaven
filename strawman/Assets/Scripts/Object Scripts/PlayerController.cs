21937c45f6cf0575995d50b55c2bca92eab42dcf
Clifford White <cjwhite@fullsail.edu>
2015-05-18 15:29:46 -0400

    Player unhookable whip implemented

--- strawman/Assets/Scripts/Object Scripts/PlayerController.cs
+++ strawman/Assets/Scripts/Object Scripts/PlayerController.cs
@@ -11,8 +11,24 @@ public class PlayerController : MonoBehaviour {
     public GameObject shieldAnchor;
     private Vector2 mousePos;
     private Vector3 screenPos;
+    //Whip
+    public float distance;
+    public float distancecheck;
+    public float MinDistance = 1.0f;
+    public bool isGrappled;
+    //draw line
+    public float startWidth = 0.05f;
+    public float endWidth = 0.05f;
+    LineRenderer line;
+    //Audio
+    public AudioSource FXSource;
+    public AudioClip DeathSound;
+    public AudioClip WhipMissSound;
+    public AudioClip WhipConnectSound;
+    public AudioClip ShieldDeflect;
 
-	// Use this for initialization
+
+    // Use this for initialization
 	void Start () {
         if (maxSpeed == 0)
             maxSpeed = 6.0f;
@@ -22,7 +38,15 @@ public class PlayerController : MonoBehaviour {
         if (shieldAnchor == null)
             shieldAnchor = GameObject.FindWithTag("ShieldAnchor");
         shieldAnchor.SetActive(false);
+        //line drawing settings
+        line = gameObject.AddComponent<LineRenderer>();
+        line.SetWidth(startWidth, endWidth);
+        line.SetVertexCount(2);
+        line.material.color = Color.red;
+        line.enabled = false;
+        //Audio Settings
 
+        
 
 	}
 	
@@ -59,38 +83,75 @@ public class PlayerController : MonoBehaviour {
         {
             shieldAnchor.transform.eulerAngles = new Vector3(shieldAnchor.transform.rotation.eulerAngles.x, shieldAnchor.transform.rotation.eulerAngles.y, Mathf.Atan2((screenPos.y - shieldAnchor.transform.position.y), -(screenPos.x - shieldAnchor.transform.position.x)) * Mathf.Rad2Deg);
         }
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isGrappled)
+        {
+
+
+
+            RaycastHit Connected;
+            Ray WhipThrown;
+            WhipThrown = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z), screenPos);
+            
+            if (Physics.Raycast(WhipThrown, out Connected, distancecheck))
+            {
+                if (Connected.collider.tag == "Hookable")
+                {
+                    WhipConnect();
+                }
+                else
+                {
+                    WhipMissed();
+                }
+            }
+        }
 	}
     void FixedUpdate() 
     {
+        if (line.enabled)
+        {
+            line.enabled = false;
+        }
         mousePos = Input.mousePosition;
         screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - Camera.main.transform.position.z));
-
-        
 
         if(!grounded)
         { return; }
         GetComponent<Rigidbody>().velocity = new Vector3(HeMoved * maxSpeed, GetComponent<Rigidbody>().velocity.y, 0.0f);
-       
-        if (Input.GetKey(KeyCode.Space) && grounded || Input.GetKey(KeyCode.W) && grounded)
-        {
-            GetComponent<Rigidbody>().AddForce(0.0f, 250.0f, 0.0f);
-            grounded = false;
-        }
 
         if (HeMoved > 0 && !FacingRight)
             Flip();
         else if (HeMoved < 0 && FacingRight)
             Flip();
 
+        if (Input.GetKey(KeyCode.Space) && grounded || Input.GetKey(KeyCode.W) && grounded)
+        {
+            GetComponent<Rigidbody>().AddForce(0.0f, 250.0f, 0.0f);
+            grounded = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            isGrappled = false;
+            line.enabled = false;
+        }
 
        
     }
     void Flip()
     {
-        FacingRight = !FacingRight;
-        Vector3 Scale = transform.localScale;
-        Scale.x *= -1;
-        transform.localScale = Scale;
+        
+        Quaternion Scale = transform.localRotation;
+        if (FacingRight)
+        {
+            Scale.y = 180;
+            FacingRight = !FacingRight;
+        }
+        else
+        {
+            Scale.y = 0;
+            FacingRight = !FacingRight;
+        }
+        transform.localRotation = Scale;
         HeMoved = 0;
     }
 
@@ -99,8 +160,33 @@ public class PlayerController : MonoBehaviour {
     {
         if (other.tag == "Fatal")
         {
-            GetComponent<AudioSource>().Play();
+            FXSource.PlayOneShot(DeathSound, 1.0f);
         }
+    }
+
+
+    void WhipConnect()
+    {
+        isGrappled = true;
+        FXSource.PlayOneShot(WhipConnectSound, 1.0f);
+
+    }
+
+    void WhipMissed()
+    {
+        distance = Vector3.Distance(screenPos, transform.position);
+
+        //sets the line positions start and end points
+        //and enables the line to be drawn
+        if (distance < distancecheck)
+        {
+            line.enabled = true;
+            line.SetPosition(0, GetComponent<Rigidbody>().transform.position);
+            line.SetPosition(1, screenPos);
+        }
+
+        FXSource.PlayOneShot(WhipMissSound, 1.0f);
+
     }
 
 }
