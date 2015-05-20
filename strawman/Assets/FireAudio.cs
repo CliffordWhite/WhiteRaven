18075a017f86f2fd9b6799615def0810bbdfa7c8
Nicholas Lee <voone97@yahoo.com>
2015-05-20 18:36:37 -0400

    fixed fire audio

---
+++ strawman/Assets/FireAudio.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireAudio : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource> ().volume = GameManager.manager.SFXVolume;
+	}
+}
