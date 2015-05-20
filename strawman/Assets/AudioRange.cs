96c94d424a8f49b3125e3ee8070c6c95f57d0d6d
Nicholas Lee <voone97@yahoo.com>
2015-05-20 16:10:55 -0400

    Stationary Flame Trap

---
+++ strawman/Assets/AudioRange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioRange : MonoBehaviour {
+	public AudioClip sound;
+	public AudioSource SFXSource;
+	float loopTime;
+
+	void Start()
+	{
+		loopTime = 0.0f;
+	}
+
+	void OnTriggerStay()
+	{
+		if (loopTime <= 0.0f) {
+			loopTime = sound.length;
+			SFXSource.PlayOneShot (sound, 0.25f);
+		}
+		loopTime -= Time.deltaTime;
+	}
+
+	void OnTriggerExit()
+	{
+		SFXSource.Stop ();
+		loopTime = 0.0f;
+	}
+}
