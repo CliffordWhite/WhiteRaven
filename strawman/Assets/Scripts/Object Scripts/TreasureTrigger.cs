400550f801efd7aefb0345c3721a9f085160bfae
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-05-20 15:30:57 -0400

    the treasure now works. yay.

--- strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
+++ strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TreasureTrigger : MonoBehaviour {
+public class TreasureTrigger : MonoBehaviour 
+{
+	public AudioSource sfxManager;	// attach the scenes SXF manager
+	public AudioClip pickupSfx;		// sound clip to play when treasure is collected
+	public GameObject deathWall;	// deathwall to activate
+	private bool collected;			// flag on when treasure collected
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		collected = false;			// initialize bool to false
+		deathWall.SetActive (false);// set activate wall to false
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter(Collider other)
+	{
+		// only trigger pickup once for player only
+		if (other.tag == "Player" && !collected)
+		{
+			collected = true;
+			sfxManager.PlayOneShot(pickupSfx, 1.0f);
+			deathWall.SetActive(true);
+			GetComponent<SpriteRenderer>().enabled = false;
+			GetComponent<ParticleSystem>().Stop();
+			Invoke ("DestroyAfterWait", 1.0f);
+		}
+	}
+	void DestroyAfterWait()
+	{
+		Destroy (gameObject);
 	}
 }
