8fc6d85aad538866564eac9d13787e5cd7c4089f
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-05-22 17:24:58 -0400

    Flame wall progress

--- strawman/Assets/Scripts/Object Scripts/FireDeathWall.cs
+++ strawman/Assets/Scripts/Object Scripts/FireDeathWall.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FireDeathWall : MonoBehaviour {
+public class FireDeathWall : MonoBehaviour 
+{
+	public Transform exitDoor;		// used to calculate where the fire will move towards
+	public float speed;				// how long in seconds it will take to get to the exit
 
-	// Use this for initialization
-	void Start () {
-	
+	private float startTime;		// used to reference when the wall starts moving
+	private Vector3 fireStart;		// keep track of origin point
+	private Vector3 fireEnd;		// keep track of end point
+
+	void Start()
+	{
+		fireStart = transform.position;
+		fireEnd = new Vector3 (fireStart.x, exitDoor.position.y, 0.0f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		float i = (Time.time - startTime)/speed; // ratio of time elapsed to overall time to complete
+		transform.position = Vector3.Lerp(fireStart,fireEnd,i);
+	}
+
+	// used to initialize the death wall
+	public void BringThePain()
+	{
+		startTime = Time.time;
+		transform.position = fireStart;
 	}
 }
--- strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
+++ strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
@@ -6,12 +6,16 @@ public class TreasureTrigger : MonoBehaviour
 	public AudioSource sfxManager;	// attach the scenes SXF manager
 	public AudioClip pickupSfx;		// sound clip to play when treasure is collected
 	public GameObject deathWall;	// deathwall to activate
-	private bool collected;			// flag on when treasure collected
+	bool collected;					// flag on when treasure collected
+	public bool Collected {
+		get {
+			return collected;
+		}
+	}
 
 	void Start () 
 	{
 		collected = false;			// initialize bool to false
-		deathWall.SetActive (false);// set activate wall to false
 	}
 	
 	void OnTriggerEnter(Collider other)
@@ -22,9 +26,10 @@ public class TreasureTrigger : MonoBehaviour
 			collected = true;
 			sfxManager.PlayOneShot(pickupSfx, 1.0f);
 			deathWall.SetActive(true);
+			deathWall.GetComponent<FireDeathWall>().BringThePain();
 			GetComponent<SpriteRenderer>().enabled = false;
 			GetComponent<ParticleSystem>().Stop();
-			Invoke ("DestroyAfterWait", 1.0f);
+			Invoke ("DestroyAfterWait", 0.5f);
 		}
 	}
 	void DestroyAfterWait()
