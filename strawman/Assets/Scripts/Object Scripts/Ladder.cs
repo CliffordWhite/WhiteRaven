199efe82365f8a2d5312dd4d2da37e2cf62a6318
Nicholas Lee <voone97@yahoo.com>
2015-05-25 22:41:49 -0400

    level 3 and ladders done level 4 needs breakable wall

--- strawman/Assets/Scripts/Object Scripts/Ladder.cs
+++ strawman/Assets/Scripts/Object Scripts/Ladder.cs
@@ -2,17 +2,40 @@
 using System.Collections;
 
 public class Ladder : MonoBehaviour {
-	public float climbSpeed;
-	public GameObject target;
+	public float speed;
+	public GameObject playerObject;
 	// Update is called once per frame
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player")
+			playerObject.GetComponent<Rigidbody> ().useGravity = false;
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.tag == "Player")
+			playerObject.GetComponent<Rigidbody> ().useGravity = true;
+	}
+
 	void OnTriggerStay(Collider other){
-		if (Input.GetKeyDown(KeyCode.W)) {
-			target.transform.position = new Vector3(target.transform.position.x,
-			                                        target.transform.position.y + (climbSpeed*Time.deltaTime));
-		}
-		if (Input.GetKeyDown(KeyCode.S)) {
-			target.transform.position = new Vector3(target.transform.position.x,
-			                                        target.transform.position.y - (climbSpeed*Time.deltaTime));
+		if (other.gameObject.tag == "Player") {
+			if (Input.GetKeyDown (KeyCode.A))
+				playerObject.transform.position = new Vector3 (playerObject.transform.position.x - speed * Time.deltaTime,
+			                                               playerObject.transform.position.y,
+			                                               playerObject.transform.position.z);
+			else if (Input.GetKeyDown (KeyCode.D))
+				playerObject.transform.position = new Vector3 (playerObject.transform.position.x + speed * Time.deltaTime,
+			                                               playerObject.transform.position.y,
+			                                               playerObject.transform.position.z);
+			else if (Input.GetKeyDown (KeyCode.W))
+				playerObject.transform.position = new Vector3 (playerObject.transform.position.x,
+			                                               playerObject.transform.position.y + speed * Time.deltaTime,
+			                                               playerObject.transform.position.z);
+			else if (Input.GetKeyDown (KeyCode.S))
+				playerObject.transform.position = new Vector3 (playerObject.transform.position.x,
+			                                               playerObject.transform.position.y - (speed * Time.deltaTime),
+			                                               playerObject.transform.position.z);
+			else
+				playerObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 0);
 		}
 	}
+
 }
