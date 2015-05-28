f92ecb5ec0027e1615805b8ab9be3a80554884df
Nicholas Lee <voone97@yahoo.com>
2015-05-28 13:02:01 -0400

    Keys and Door are a thing now

--- strawman/Assets/Scripts/Object Scripts/LockedDoor.cs
+++ strawman/Assets/Scripts/Object Scripts/LockedDoor.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class LockedDoor : MonoBehaviour {
+	public AudioClip sound;
+	public AudioSource SFXSource;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player") {
+			if (GameManager.manager.keys >= 1){
+				GameManager.manager.keys--;
+				SFXSource.PlayOneShot(sound,1.0f);
+				gameObject.SetActive(false);
+				GameManager.manager.keyShowTime = 2.0f;
+			}
+		}
 	}
 }
