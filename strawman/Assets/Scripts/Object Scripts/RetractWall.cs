840877cf088bc8df2d73786380f3dba8f0135c2f
mos99z <austinlware@gmail.com>
2015-06-02 20:52:11 -0400

    Finished: Retracting Platform

--- strawman/Assets/Scripts/Object Scripts/RetractWall.cs
+++ strawman/Assets/Scripts/Object Scripts/RetractWall.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class RetractWall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
+	[Header ("Movement Speed, try small numebers like 0.01")]
+	public float MoveSpeed = 0.0f;
+	[Header ("Don't Forget to set and end position")]
+	public Vector3 EndLocation = new Vector3(0, 0, 0);
+	public bool Moving = false;
+	private AudioSource SFXPlayer = null;
+	
+	void Start ()
+	{
+		SFXPlayer = GetComponent<AudioSource>();
+		if( Moving )
+			SFXPlayer.Play();
+		EndLocation.z = transform.position.z;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
+		if( Moving && !SFXPlayer.isPlaying )
+			SFXPlayer.Play();
+		else if( !Moving && SFXPlayer.isPlaying )
+				SFXPlayer.Stop();
+	}
 	
+	void FixedUpdate ()
+	{
+		Vector3 oldPos = transform.position;
+		if (Moving)
+			transform.position = Vector3.MoveTowards( transform.position, EndLocation, MoveSpeed );
+		if (oldPos == transform.position)
+			Moving = false;
+	}
+	
+	void Trigger()
+	{
+		Moving = true;
+		SFXPlayer.Play();
 	}
 }
