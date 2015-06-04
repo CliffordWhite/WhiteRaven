5656995e9256effa3a7a9ac4b5ceefbf5bc553d4
Nicholas Lee <voone97@yahoo.com>
2015-06-04 15:01:53 -0400

    consolidation treasure collect and trigger

--- strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
+++ strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
@@ -14,7 +14,7 @@ public class TreasureTrigger : MonoBehaviour
      float shakeAmount = 0.1f;
      float decreaseFactor = 1.0f;
      //Player Pos
-   //  GameObject Player;
+     GameObject Player;
 
 
 
@@ -36,7 +36,7 @@ public class TreasureTrigger : MonoBehaviour
 
     void Update()
     {
-      /*  if (shake > 0)
+        if (shake > 0)
         {
             _Camera.transform.localPosition = new Vector3((Random.Range(0.1f, 5.0f) * shakeAmount) + Player.transform.position.x, Player.transform.position.y, -10.0f);
             shake -= Time.deltaTime * decreaseFactor;
@@ -46,7 +46,7 @@ public class TreasureTrigger : MonoBehaviour
         {
             shake = 0.0f;
             _Camera.transform.localPosition = new Vector3(Player.transform.localPosition.x, Player.transform.localPosition.y, -10.0f);
-        }*/
+        }
     }
 
 	void OnTriggerEnter(Collider other)
@@ -55,16 +55,20 @@ public class TreasureTrigger : MonoBehaviour
 		if (other.tag == "Player" && !collected)
 		{
 			collected = true;
-			sfxManager.PlayOneShot(pickupSfx, 1.0f);
-			deathWall.SetActive(true);
-			deathWall.GetComponent<FireDeathWall>().BringThePain();
 			GetComponent<SpriteRenderer>().enabled = false;
 			GetComponent<ParticleSystem>().Stop();
-			musicSource.GetComponent<MusicChange>().PlayHasteMusic();
+			if (gameObject.tag == "MainTreasure"){
+				sfxManager.PlayOneShot(pickupSfx, 1.0f);
+				deathWall.SetActive(true);
+				deathWall.GetComponent<FireDeathWall>().BringThePain();
+				musicSource.GetComponent<MusicChange>().PlayHasteMusic();
+				GameManager.manager.DoorUnlocked = true;
+				shake = 0.5f;
+			}
+			else {
+				GameManager.manager.secretGot = true;
+			}
 			Invoke ("DestroyAfterWait", 0.5f);
-			GameManager.manager.DoorUnlocked = true;
-            //shake = 0.5f;
-
 		}
 	}
 	void DestroyAfterWait()
