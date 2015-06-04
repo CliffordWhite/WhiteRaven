fc72f8ff4f01c5377bfadad12155ec8d85159053
Clifford White <cjwhite@fullsail.edu>
2015-06-04 14:30:07 -0400

    Camera

--- strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
+++ strawman/Assets/Scripts/Object Scripts/TreasureTrigger.cs
@@ -9,7 +9,7 @@ public class TreasureTrigger : MonoBehaviour
 	public GameObject musicSource;	// access music change script on pickup
 
     //Camera
-     GameObject camera; // set this via inspector
+     GameObject _Camera; // set this via inspector
      float shake = 0.0f;
      float shakeAmount = 0.1f;
      float decreaseFactor = 1.0f;
@@ -30,7 +30,7 @@ public class TreasureTrigger : MonoBehaviour
 	void Start () 
 	{
 		collected = false;			// initialize bool to false
-        camera = GameObject.FindWithTag("MainCamera");
+        _Camera = GameObject.FindWithTag("MainCamera");
         Player = GameObject.FindWithTag("Player");
 	}
 
@@ -38,14 +38,14 @@ public class TreasureTrigger : MonoBehaviour
     {
         if (shake > 0)
         {
-            camera.transform.localPosition = Player.transform.position * shakeAmount;
+            _Camera.transform.localPosition = new Vector3((Random.Range(0.1f, 5.0f) * shakeAmount) + Player.transform.position.x, Player.transform.position.y, -10.0f);
             shake -= Time.deltaTime * decreaseFactor;
 
         }
         else if(shake < 0)
         {
             shake = 0.0f;
-            camera.transform.localPosition = new Vector3(Player.transform.localPosition.x,Player.transform.localPosition.y,-10.0f);
+            _Camera.transform.localPosition = new Vector3(Player.transform.localPosition.x, Player.transform.localPosition.y, -10.0f);
         }
     }
 
