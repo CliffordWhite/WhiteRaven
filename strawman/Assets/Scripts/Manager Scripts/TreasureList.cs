5e7399e1fe61e87ac9f7ba0fcdfd0a186ec5e277
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-06-16 20:35:04 -0400

    still working on it

--- strawman/Assets/Scripts/Manager Scripts/TreasureList.cs
+++ strawman/Assets/Scripts/Manager Scripts/TreasureList.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TreasureList : MonoBehaviour 
 {
@@ -19,7 +20,8 @@ public class TreasureList : MonoBehaviour
 	public Sprite level8b;
 	public Sprite level9a;
 	public Sprite level9b;
-
+	public List<Sprite> treasures;
+	
 	[Header ("Set bonus treasure sprite here")]
 	public Sprite lvl2;
 	public Sprite lvl4;
@@ -28,5 +30,30 @@ public class TreasureList : MonoBehaviour
 	public Sprite lvl7;
 	public Sprite lvl8;
 	public Sprite lvl9;
+	public List<Sprite> secrets;
 
+	void Start()
+	{
+		treasures.Add(level4);
+		treasures.Add(level5a);
+		treasures.Add(level5b);
+		treasures.Add(level6a);
+		treasures.Add(level6b);
+		treasures.Add(level6c);
+		treasures.Add(level7a);
+		treasures.Add(level7b);
+		treasures.Add(level7c);
+		treasures.Add(level8a);
+		treasures.Add(level8b);
+		treasures.Add(level9a);
+		treasures.Add(level9b);
+		
+		secrets.Add(lvl2);
+		secrets.Add(lvl4);
+		secrets.Add(lvl5);
+		secrets.Add(lvl6);
+		secrets.Add(lvl7);
+		secrets.Add(lvl8);
+		secrets.Add(lvl9);
+	}
 }
---
+++ strawman/Assets/Scripts/Menu Scripts/TreasurePause.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TreasurePause : MonoBehaviour 
+{
+	[Header ("Set each level's treasure icon here")]
+	public GameObject level4;
+	public GameObject level5a;
+	public GameObject level5b;
+	public GameObject level6a;
+	public GameObject level6b;
+	public GameObject level6c;
+	public GameObject level7a;
+	public GameObject level7b;
+	public GameObject level7c;
+	public GameObject level8a;
+	public GameObject level8b;
+	public GameObject level9a;
+	public GameObject level9b;
+	public List<GameObject> treasures;
+
+	[Header ("Set bonus treasure icon here")]
+	public GameObject lvl2;
+	public GameObject lvl4;
+	public GameObject lvl5;
+	public GameObject lvl6;
+	public GameObject lvl7;
+	public GameObject lvl8;
+	public GameObject lvl9;
+	public List<GameObject> secrets;
+
+	void Start()
+	{
+		treasures.Add(level4);
+  		treasures.Add(level5a);
+  		treasures.Add(level5b);
+  		treasures.Add(level6a);
+  		treasures.Add(level6b);
+  		treasures.Add(level6c);
+  		treasures.Add(level7a);
+  		treasures.Add(level7b);
+  		treasures.Add(level7c);
+  		treasures.Add(level8a);
+  		treasures.Add(level8b);
+  		treasures.Add(level9a);
+  		treasures.Add(level9b);
+
+		secrets.Add(lvl2);
+		secrets.Add(lvl4);
+		secrets.Add(lvl5);
+		secrets.Add(lvl6);
+		secrets.Add(lvl7);
+		secrets.Add(lvl8);
+		secrets.Add(lvl9);
+
+		for (int i = 0; i < GameManager.manager.treasureCollected.Length; i++)
+		{
+			if (i >= treasures.Count) continue;
+			if (GameManager.manager.treasureCollected[i])
+				treasures[i].GetComponent<Image>().sprite = GameManager.manager.GetComponent<TreasureList>().treasures[i];
+		}
+		for (int i = 0; i < GameManager.manager.secrettreasureCollected.Length; i++)
+		{
+			if (i >= secrets.Count) continue;
+			if (GameManager.manager.secrettreasureCollected[i])
+				secrets[i].GetComponent<Image>().sprite = GameManager.manager.GetComponent<TreasureList>().secrets[i];
+		}
+	}
+}
