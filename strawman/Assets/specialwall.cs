b9506597a40bf31dbc08a0e55703bbd748108819
Nicholas Lee <voone97@yahoo.com>
2015-06-18 16:36:04 -0400

    secret treasures

---
+++ strawman/Assets/specialwall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class specialwall : MonoBehaviour {
+
+	public GameObject[] levers;
+	public int limit;
+	public Vector3 tarPos;
+	// Update is called once per frame
+	void Update () {
+		if (limit < 5) {
+			limit = 0;
+			for (int i = 0; i < levers.Length; i++) {
+				limit += levers [i].GetComponent<Lever> ().hastriggered;
+			}
+		} else
+			transform.position = Vector3.MoveTowards (transform.position, tarPos, 0.25f);
+	}
+}
