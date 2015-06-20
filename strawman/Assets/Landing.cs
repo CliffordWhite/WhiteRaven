8f7d013e004142ab3b40ef2f9cb3e9c3f864ede9
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-06-20 14:54:13 -0400

    Made some minor changes for an updated build for QA team

---
+++ strawman/Assets/Landing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Landing : MonoBehaviour 
+{
+	public AudioSource sfxManager;
+	public AudioClip landing;
+	public ParticleSystem dust;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Floor" || other.tag == "Wall")
+		{
+			sfxManager.PlayOneShot(landing);
+			Object thing = Instantiate(dust, transform.position, transform.rotation);
+			Destroy(thing, 0.5f);
+		}
+	}
+
+
+}
