40224667303cbce004c1c17e3437c8d42e5fac0f
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-06-22 00:05:36 -0400

    Fixed issue with minecart being able to leave rails at start of level 9. Put invisible kill box to deter player from doing so

---
+++ strawman/Assets/MinecartKillZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinecartKillZone : MonoBehaviour 
+{
+	public GameObject player;	//attach player to make sending kill message easy
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "MineCart")
+			player.SendMessage("KillPlayer");
+	}
+}
